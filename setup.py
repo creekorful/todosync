6e1a9a5c539fe0df758cf91b1ee44b5fb5be24d6
Aloïs Micard <alois@micard.lu>
2021-02-26 09:48:39 +0100

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="todosync",
-    version="0.1.1",
+    version="0.1.2",
     author="Aloïs Micard",
     author_email="alois@micard.lu",
     description="Synchronize your Git{hub,lab} issues with Todoist.",
